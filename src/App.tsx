aac3a1e5f9260a9ac6bcb5e16a847ee9e8b02c49
John Kuss <143646569+JohnKuss@users.noreply.github.com>
2024-09-25 22:29:31 -0400

    Added DoubleHalf to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,8 @@ function App(): React.JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
+            <hr />
+            <DoubleHalf></DoubleHalf>
         </div>
     );
 }
