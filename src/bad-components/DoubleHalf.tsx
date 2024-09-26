6e797bbbc11c7ba063b4f722f24ec2baff6d29bc
John Kuss <143646569+JohnKuss@users.noreply.github.com>
2024-09-25 22:25:21 -0400

    Reworked DoubleHalf.tsx to utilize interface.

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,38 +1,45 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface DoubleHalfProps {
+    setDhValue: (newDhValue: number) => void;
+    dhValue: number;
+}
+
+function Doubler({ setDhValue, dhValue }: DoubleHalfProps): React.JSX.Element {
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(2 * dhValue);
+            }}
+        >
+            Double
+        </Button>
+    );
+}
+
+function Halver({ setDhValue, dhValue }: DoubleHalfProps): React.JSX.Element {
+    return (
+        <Button
+            onClick={() => {
+                setDhValue(0.5 * dhValue);
+            }}
+        >
+            Halve
+        </Button>
+    );
+}
+
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
-    function Doubler(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setDhValue(2 * dhValue);
-                }}
-            >
-                Double
-            </Button>
-        );
-    }
-    function Halver(): React.JSX.Element {
-        return (
-            <Button
-                onClick={() => {
-                    setDhValue(0.5 * dhValue);
-                }}
-            >
-                Halve
-            </Button>
-        );
-    }
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
+            <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
         </div>
     );
 }
